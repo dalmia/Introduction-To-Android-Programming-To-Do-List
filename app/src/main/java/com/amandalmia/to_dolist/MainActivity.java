68b5e9d8b2dc06775beea4908e9c65c37b92feba
dalmia <amandalmia18@gmail.com>
2016-04-11 08:56:11 +0530

    Storing User input

--- app/src/main/java/com/amandalmia/to_dolist/MainActivity.java
+++ app/src/main/java/com/amandalmia/to_dolist/MainActivity.java
@@ -2,12 +2,36 @@ package com.amandalmia.to_dolist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //declare the variables
+    EditText input;
+    Button addNote;
+    LinearLayout notes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //link the variables with their ids
+        addNote = (Button) findViewById(R.id.add_note);
+        input = (EditText) findViewById(R.id.input);
+        notes = (LinearLayout) findViewById(R.id.notes);
+        addNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Storing and showing the user data
+                Toast.makeText(MainActivity.this, input.getText().toString(), Toast.LENGTH_SHORT).show();
+                //Remove whatever was written once add is pressed.
+                input.setText("");
+            }
+        });
     }
 }
